30e36e164fe60eadcdba3b98b149aa423b0bd4c3
JuanMmonte <juanmaniac96@gmail.com>
2026-01-27 21:11:56 -0500

    purchase modal change language configuration

--- src/components/PurchaseModal.jsx
+++ src/components/PurchaseModal.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 export default function PurchaseModal({ isOpen, onClose, pkg, onConfirm }) {
+
+  const { t } = useTranslation();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [telephone, setTelephone] = useState("");
@@ -11,13 +14,13 @@ export default function PurchaseModal({ isOpen, onClose, pkg, onConfirm }) {
     <div className="fixed inset-0 bg-[#333333] bg-opacity-70 flex items-center justify-center z-50 p-3 sm:p-4">
       <div className="bg-[#fff8e1] rounded-lg sm:rounded-xl shadow-lg w-full max-w-md p-4 sm:p-6 relative">
         <h2 className="text-lg sm:text-xl font-bold mb-3 sm:mb-4 text-[var(--color-text)] text-center">
-          Comprar {pkg.name_package}
+          {t("purchase.title")} {pkg.name_package}
         </h2>
         <h3 className="text-center text-base sm:text-lg font-semibold text-[var(--color-text)] mb-2">
-          Descripción: {pkg.description_package}
+          {t("purchase.description")}: {pkg.description_package}
         </h3>
         <h3 className="mb-3 sm:mb-4 text-center text-lg sm:text-xl font-semibold text-[var(--color-text )]">
-          Precio: <span className="text-[var(--color-primary)]">{pkg.price_package} EUR</span>
+          {t("purchase.price")}: <span className="text-[var(--color-primary)]">{pkg.price_package} EUR</span>
         </h3>
         <form
           onSubmit={(e) => {
@@ -28,7 +31,7 @@ export default function PurchaseModal({ isOpen, onClose, pkg, onConfirm }) {
         >
           <div>
             <label className="block text-[#333333] text-xs sm:text-sm font-medium mb-1 sm:mb-2">
-              Nombre completo
+              {t("purchase.name")}
             </label>
             <input
               type="text"
@@ -41,7 +44,7 @@ export default function PurchaseModal({ isOpen, onClose, pkg, onConfirm }) {
 
           <div>
             <label className="block text-[#333333] text-xs sm:text-sm font-medium mb-1 sm:mb-2">
-              Correo electrónico
+              {t("purchase.email")}
             </label>
             <input
               type="email"
@@ -51,9 +54,9 @@ export default function PurchaseModal({ isOpen, onClose, pkg, onConfirm }) {
               className="w-full bg-[#2c2c2c] text-[#fff8e1] border border-gray-300 rounded-lg p-2 sm:p-2.5 text-xs sm:text-sm focus:outline-none focus:ring-2 focus:ring-[#ffc107]"
             />
           </div>
-           <div>
+          <div>
             <label className="block text-[#333333] text-xs sm:text-sm font-medium mb-1 sm:mb-2">
-              Telefono
+              {t("purchase.phone")}
             </label>
             <input
               type="number"
@@ -71,14 +74,14 @@ export default function PurchaseModal({ isOpen, onClose, pkg, onConfirm }) {
               onClick={onClose}
               className="px-3 sm:px-4 py-2 bg-[#333333] text-[#ffc107] rounded-2xl sm:rounded-3xl font-bold hover:bg-gray-400 text-xs sm:text-sm order-2 sm:order-1"
             >
-              CANCELAR
+              {t("purchase.cancel")}
             </button>
 
             <button
               type="submit"
               className="px-3 sm:px-4 py-2 bg-[#ffc107] text-[#333333] rounded-2xl sm:rounded-3xl hover:bg-[#ffb300] font-bold text-xs sm:text-sm order-1 sm:order-2"
             >
-              COMPRAR
+              {t("purchase.buy")}
             </button>
           </div>
         </form>
